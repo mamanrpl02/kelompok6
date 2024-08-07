5b89648684daf24da29929f731592af9fe805ad0
Abdurrahman <145602729+mamanrpl02@users.noreply.github.com>
2024-08-07 10:10:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projek Kelompok 6</title>
+</head>
+<body>
+    <h1>kelompok 6</h1>
+    <p>Anggota Kelompok</p>
+    <ol>
+        <li> Abdurrahmamn</li>
+        <li> Malini</li>
+        <li> Heni Suenni</li>
+        <li> Marsela</li>
+    </ol>
+</body>
+</html>
